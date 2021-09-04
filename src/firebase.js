85bc0655f559fa142a1d43b01006c20c92d665ad
looky03 <vajearvin26@gmail.com>
2021-09-05 01:59:05 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAOzJAsodWuJYYh9cIlxflJ3l4zr_8DONU",
+    authDomain: "slack-clone-9bb02.firebaseapp.com",
+    projectId: "slack-clone-9bb02",
+    storageBucket: "slack-clone-9bb02.appspot.com",
+    messagingSenderId: "971812640225",
+    appId: "1:971812640225:web:ba278ba83691d5870b6ea0",
+    measurementId: "G-N5VCTNW85Y"
+  };
